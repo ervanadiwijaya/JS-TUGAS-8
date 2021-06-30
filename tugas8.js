0ced02434265aaf6e4f27578ad8795d0aaba4f86
jauhhari <ervanadiwijaya@gmail.com>
2021-06-30 15:35:07 +0700

    OK

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+function dataFilm(params) {
+    var film = {
+        judul : 'the avengers end game',
+        durasi : 120,
+        genre : 'action',
+        studio : 'marvel cinematic studio'
+    }
+
+    film.durasi = 140;
+    film.tahun = 2019;
+    console.log(film);
+    
+}
+
+dataFilm();
